8d03d22287d429d2035ccc165e20568f536672d9
Nathan Bergey <nathan.bergey@gmail.com>
2014-12-24 01:18:47 -0800

    open websocket example on endpoint side

--- director/js/controller.js
+++ director/js/controller.js
@@ -12,10 +12,10 @@ var connect = function (uri, $scope) {
 gsdApp.controller('GroundSystemsCtrl', function ($scope) {
 
     var machines =  [
-        {'name': "LTC", 'connect': "ws://ltc.psas.ground:4000"},
-        {'name': "Telemetry Server", 'connect': "ws://telem.psas.ground:4000"},
-        {'name': "Trackmaster", 'connect': "ws://tm3k.psas.ground:4000"},
-        {'name': "Ground Master Controller", 'connect': "ws://localhost:4000"},
+        {'name': "LTC", 'connect': "ws://ltc.psas.ground:8000"},
+        {'name': "Telemetry Server", 'connect': "ws://telem.psas.ground:8000"},
+        {'name': "Trackmaster", 'connect': "ws://tm3k.psas.ground:8000"},
+        {'name': "Ground Master Controller", 'connect': "ws://localhost:8000"},
     ];
 
     $scope.machines = machines;
