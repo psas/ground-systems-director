abb9cb77a3f006d43e337582e4d7b5c1ed054395
Nathan Bergey <nathan.bergey@gmail.com>
2014-12-26 17:23:10 -0800

    documentation in controller.js, begin writting commands

--- director/js/controller.js
+++ director/js/controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
+/* javascipt junk to bind a useful context to callback functions */
 var proxy = function(fn, context) {
   return function() {
     return fn.apply(context, arguments);
   };
 };
 
+/* Write our own filter to change true|false to UX friendly strings */
 angular.module("gsdApp.controllers").filter('connected', function() {
     return function(text, length, end) {
         if (text) {
@@ -16,8 +18,12 @@ angular.module("gsdApp.controllers").filter('connected', function() {
 });
 
 
+/**
+ * Main Controller
+ */
 angular.module("gsdApp.controllers").controller('gsdCtrl', ['$rootScope', '$scope','$websocket', function ($rootScope, $scope, $websocket) {
 
+    //  Machines we know about, and their locations on the network
     var machines =  [
         //{'name': "LTC", 'connect': "ws://ltc.psas.ground:8000"},
         {'name': "Telemetry Server", 'connect': "ws://telem.psas.ground:8000"},
@@ -25,13 +31,26 @@ angular.module("gsdApp.controllers").controller('gsdCtrl', ['$rootScope', '$scop
         {'name': "Ground Master Controller", 'connect': "ws://localhost:8000"},
     ];
 
+    // place initial list into the scope
     $scope.machines = machines;
 
-
+    /**
+     * For each machine in the list, we will set up the websocket, events, and
+     * callbacks to run the show.
+     */
     for (var i=0; i<machines.length; i++) {
+
+        // current machine
         var machine = $scope.machines[i];
+
+        // default connection status
         machine.connected = false;
+
+        // Init websocket!
         machine.ws = $websocket.$new(machine.connect);
+
+        /* WEBSOCKET EVENTS: */
+        // OPEN
         machine.ws.$on('$open', proxy(function () {
             console.log('connect to '+this.name);
 
@@ -39,16 +58,34 @@ angular.module("gsdApp.controllers").controller('gsdCtrl', ['$rootScope', '$scop
                 this.connected = true;
             }, this));
 
+            // on connect, send the `list` command
             this.ws.$emit('list');
         }, machine));
 
+        // CLOSE
+        machine.ws.$on('$close', proxy(function () {
+            console.log(this.name+' disconnect');
+
+            $rootScope.$apply(proxy(function() {
+                this.connected = false;
+            }, this));
+        }, machine));
+
+        /* Local Events (Buttons) */
+        // REBOOT
+        machine.reboot = proxy(function() {
+            console.log("reboot");
+            this.ws.$emit('reboot');
+        }, machine);
+
+        /* Command Handlers */
+        // LIST
         machine.ws.$on('list', proxy(function (data) {
             console.log('got list from '+this.name);
             $rootScope.$apply(proxy(function() {
                 this.services = data;
             }, this));
         }, machine));
-
     }
 
 }]);
